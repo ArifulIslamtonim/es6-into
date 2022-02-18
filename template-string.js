464c0e55983502663f7d48d13ab751385dd6feec
AriulIslamtonim <arifulislamtonim@gmail.com>
2022-02-19 00:45:56 +0600

    template

---
+++ template-string.js
@@ -0,0 +1,10 @@
+const priya = 'Asif Akbar';
+const meye = "Meye Tumi dukkho chino";
+const kobita = `kobita tumi sopno charini`;
+const multiLine = `This is my first line`;
+
+const labu = 'mamur beta';
+const how = 'nanir beta';
+
+const new1 = `this is you: ${labu} ${how}. tr nanir p***. ${(12 * 112) / 10}`;
+console.log(new1);
